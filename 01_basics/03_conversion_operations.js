373056384efbd1f556b9579fe4bbab6f208397eb
Priyanshu Harshvardhan <priyanshuharshvardhan21@gmail.com>
2026-01-30 15:58:29 +0530

    Implement conversion and arithmetic operations
    Added various arithmetic and string operations to demonstrate conversion and manipulation.

--- 01_basics/03_conversion_operations.js
+++ 01_basics/03_conversion_operations.js
@@ -24,3 +24,28 @@ let string_number=String(some_number);
 console.log(typeof string_number);
 console.log(string_number);
 
+//**********operations */
+let value =3
+let negValue= -value
+console.log(negValue);
+console.log(2**3); //2 to power 3
+console.log(2%3); //remainder
+
+let str1="hello"
+let str2 = " priyanshu"
+let str3=str1+ str2;
+console.log(str3);
+
+
+console.log("1"+2+2); //122 as string
+console.log(1+2+"3"); //returns 33 as string
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let counter=100
+counter++;
+console.log(counter);
+++counter;
+console.log(counter);
